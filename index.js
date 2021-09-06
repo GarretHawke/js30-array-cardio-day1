43e5724e2463e3fb9ba7e4cfd5c82f027daeed17
GarretHawke <alexandra17au@gmail.com>
2021-09-06 20:37:38 +0300

    feat: add task8 solution

--- index.js
+++ index.js
@@ -74,9 +74,17 @@ const alpha = people.sort((lastOne, nextOne) => {
   const [bLast, bFirst] = nextOne.split(', ');
   return aLast > bLast ? 1 : -1;
 });
-
-console.table(alpha);
+console.log(alpha);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+const transportation = data.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+console.log(transportation);
